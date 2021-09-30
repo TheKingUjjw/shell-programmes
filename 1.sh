bf6a1c9b19cd473419ca6e2a11d8275ae2f069c5
thomasajai <53503024+thomasajai@users.noreply.github.com>
2021-10-01 00:52:03 +0530

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,10 @@
+# Write a shell script that accepts one or more file name as arguments and
+# convert the file contents to uppercase, provided they exist in the current
+# directory
+
+#! /usr/bin/bash
+
+a=1; b=2; c=3;
+d=$(( ++a**b*c++ + a ))
+echo $d
+exit 0
